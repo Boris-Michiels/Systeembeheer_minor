630695a7c84f663c26c4adb326360ed2a4cdf2b4
Boris Michiels <boris.michiels@uclllabs.be>
2022-07-05 22:25:35 +0200

    Update offline backup script to create a folder if it doesn't exist

--- scripts/offline_backup.sh
+++ scripts/offline_backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync -a --delete -e 'ssh -p 22345' /etc r0760222@leia.uclllabs.be:/home/LDAP/r0760222/sysbeh/backup/
+ssh -p 22345 r0760222@leia.uclllabs.be 'mkdir -p /home/LDAP/r0760222/sysbeh/backup/' && rsync -a --delete -e 'ssh -p 22345' /etc r0760222@leia.uclllabs.be:/home/LDAP/r0760222/sysbeh/backup
+/
