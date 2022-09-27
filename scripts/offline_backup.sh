8be8307ddb49d50d9c0dd2ead26446ff8da17655
Boris Michiels <boris.michiels@uclllabs.be>
2022-09-27 16:44:38 +0200

    Update offline backup script to delete removed files

--- scripts/offline_backup.sh
+++ scripts/offline_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -p 22345 r0760222@leia.uclllabs.be 'mkdir -p /home/LDAP/r0760222/sysbeh/backup/' && rsync -a -e 'ssh -p 22345' /etc r0760222@leia.uclllabs.be:/home/LDAP/r0760222/sysbeh/backup/
+ssh -p 22345 r0760222@leia.uclllabs.be 'mkdir -p /home/LDAP/r0760222/sysbeh/backup/' && rsync -a --delete -e 'ssh -p 22345' /etc r0760222@leia.uclllabs.be:/home/LDAP/r0760222/sysbeh/backup/
