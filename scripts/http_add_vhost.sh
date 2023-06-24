7a3f26e8fd011fbb88c5225d02e95d899ce320f8
Boris Michiels <boris.michiels@uclllabs.be>
2023-06-25 00:25:19 +0200

    update script

--- scripts/http_add_vhost.sh
+++ scripts/http_add_vhost.sh
@@ -9,5 +9,17 @@ VHOST=$(echo "$1" | cut -d "." -f1)
 SUBDOMAIN=$(echo "$1" | cut -d "." -f2)
 DBFILE="db.$SUBDOMAIN.boris-michiels.sb.uclllabs.be"
 
-echo $VHOST
-echo $SUBDOMAIN
+mkdir -p /var/www/html/$SUBDOMAIN/$VHOST
+
+echo "<VirtualHost *:80>
+        ServerName $VHOST.$SUBDOMAIN.boris-michiels.sb.uclllabs.be
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html/$SUBDOMAIN/$VHOST
+        ErrorLog ${APACHE_LOG_DIR}/$VHOST.$SUBDOMAIN-error.log
+        CustomLog ${APACHE_LOG_DIR}/$VHOST.$SUBDOMAIN-access.log combined
+</VirtualHost>" > /etc/apache2/sites-available/mrt.$VHOST.$SUBDOMAIN.conf
+
+echo "welcome $VHOST.$SUBDOMAIN" > /var/www/html/$SUBDOMAIN/$VHOST/index.html
+
+a2ensite mrt.$VHOST.$SUBDOMAIN.conf
+systemctl reload apache2
