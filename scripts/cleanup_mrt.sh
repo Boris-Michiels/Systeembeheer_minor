8d3abd4a328e2d9062d5924e82109a7116135845
Boris Michiels <boris.michiels@uclllabs.be>
2023-06-27 15:18:39 +0200

    update cleanup script

--- scripts/cleanup_mrt.sh
+++ scripts/cleanup_mrt.sh
@@ -3,7 +3,7 @@
 sed -i '/^subzone/d' /etc/bind/db.boris-michiels.sb.uclllabs.be
 cat /etc/bind/db.boris-michiels.sb.uclllabs.be > /etc/bind/db.boris-michiels.sb.uclllabs.be.back
 rm -r /etc/bind/zones
-echo "" > named.conf.mrt-zones
+echo "" > /etc/bind/named.conf.mrt-zones
 
 rm /etc/apache2/sites-enabled/mrt.*
 rm /etc/apache2/sites-available/mrt.*
