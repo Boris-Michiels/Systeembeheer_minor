31bf811674574547e885726524b9e44262c76751
Boris Michiels <boris.michiels@uclllabs.be>
2023-07-02 19:28:59 +0200

    update

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -6,18 +6,10 @@ start () {
 }
 
 stop () {
-	iptables-save > /etc/iptables/rules.v4
-	ip6tables-save > /etc/iptables/rules.v6
 	iptables -F
 	iptables -X
-	iptables -P INPUT   ACCEPT
-	iptables -P FORWARD ACCEPT
-	iptables -P OUTPUT  ACCEPT
 	ip6tables -F
 	ip6tables -X
-	ip6tables -P INPUT   ACCEPT
-	ip6tables -P FORWARD ACCEPT
-	ip6tables -P OUTPUT  ACCEPT
 }
 
 case "$1" in
