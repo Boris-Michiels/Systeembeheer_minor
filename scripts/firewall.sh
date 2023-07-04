143dd7f21983a1921b79ecd2b99be840ca0a9eb6
Boris Michiels <boris.michiels@uclllabs.be>
2023-07-04 23:32:53 +0200

    firewall

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -3,15 +3,24 @@
 start () {
 	iptables-restore < /etc/iptables/rules.v4
 	ip6tables-restore < /etc/iptables/rules.v6
+
+	sysctl -w net.ipv4.ip_forward=0
+	sysctl -w net.ipv6.conf.all.forwarding=0
+	sysctl -w net.ipv4.tcp_syncookies=1
 }
 
 stop () {
 	iptables -F
 	iptables -X
 	iptables -P INPUT ACCEPT
+	iptables -P FORWARD ACCEPT
+	iptables -P OUTPUT ACCEPT
 	ip6tables -F
 	ip6tables -X
 	ip6tables -P INPUT ACCEPT
+	ip6tables -P INPUT ACCEPT
+	ip6tables -P FORWARD ACCEPT
+	ip6tables -P OUTPUT ACCEPT
 }
 
 case "$1" in
